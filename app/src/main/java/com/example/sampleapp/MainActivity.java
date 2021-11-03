2b55bfde19e7df21a882a1fe4d154c863b31f5a3
harishteddy <rudrurlt@gmail.com>
2021-11-03 09:09:54 +0530

    added button

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-          //changes done by hari
+          //changes done by harish reddy please
 
         //addded comment
 
